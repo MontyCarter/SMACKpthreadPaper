81062baa609084f741f3174856e7ee471ca0812a
mcarter <montgomery.carter@gmail.com>
2015-03-27 15:07:10 -0600

    Initial commit.  UGThesis is currently just a copy of UGThesisProposal

---
+++ Proposal/auto/UGThesisProposal_MontgomeryCarter.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook "UGThesisProposal_MontgomeryCarter"
+ (lambda ()
+    (LaTeX-add-bibliographies)
+    (TeX-run-style-hooks
+     "geometry"
+     "right=0.5in"
+     "left=0.5in"
+     "bottom=0.5in"
+     "top=0.5in"
+     "cite"
+     "graphicx"
+     "parskip"
+     "amsthm"
+     "amssymb"
+     "palatino"
+     "eulervm"
+     "amsmath"
+     "latex2e"
+     "scrartcl10"
+     "scrartcl"
+     "")))
+
